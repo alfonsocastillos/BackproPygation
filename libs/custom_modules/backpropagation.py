50ecf0c1dffdddc4e37866b56a84bfb44cadefc7
alfonsocastillos <alfonso.csrz@gmail.com>
2025-06-10 17:06:17 -0600

    feat(backpropagation): added forward calculation

--- libs/custom_modules/backpropagation.py
+++ libs/custom_modules/backpropagation.py
@@ -1,43 +1,64 @@
 import numpy as np
 import logging
+from typing import Callable
 
 from custom_modules import utils
+from custom_modules import math_utils
 
 class Backpropagation():
-    __batch_size: int
     __input_layer: int
     __hidden_layers: int
     __output_layer: int
-    __data_dir: str
+    __out_pattern: dict
     __images: list
     __labels: np.ndarray
     __neurons: list[np.ndarray]
     __weights: list[np.ndarray]
     __biases: list[np.ndarray]
+    __act_function: Callable[[int | float | np.ndarray], float | np.ndarray]
+    __act_deriv: Callable[[int | float | np.ndarray], float | np.ndarray]
     __logger: logging.Logger = logging.getLogger(__name__)
 
-    def __init__(self, batch_size: int=10, hidden_layers: int=1, output_layer: int=10, data_dir: str='dataset') -> None:
+    def __init__(self, out_pattern: dict, hidden_layers: int=1, output_layer: int=10, act_function: str='sigmoid', ) -> None:
         '''
         Constructor for the Backpropagation class.
 
-        Args:
-            batch_size (optional): The number of training examples used before modifying weights. Default: 10.
+        Args:            
+            out_pattern: The output pattern associated with each label.
             hidden_layers (optional): The number of hidden layers inside the ANN. Default: 1.
-            data_dir (optional): The path to the directory containing the training and testing data. Default='dataset'.        
+            output_layer (optional): The number of neurons that form the output layer. Default: 10.                        
+            act_function (optional): The activation function for the neurons. Allowed values: sigmoid, tanh, ReLU. Default: sigmoid.
         '''
-        self.__batch_size = batch_size
         self.__hidden_layers = hidden_layers
         self.__output_layer = output_layer
-        self.__data_dir = data_dir
+        self.__out_pattern = out_pattern
+        match act_function.lower():
+            case 'sigmoid':
+                self.__act_function = math_utils.sigmoid
+                self.__act_deriv = math_utils.d_sigmoid
+            case 'tanh':
+                self.__act_function = math_utils.tanh
+                self.__act_deriv = math_utils.d_tanh
+            case 'relu':
+                self.__act_function = math_utils.relu
+                self.__act_deriv = math_utils.d_relu
+            case _:
+                self.__logger.error('Unknown activation function.')
+                self.__logger.error('Activation must be one of: sigmoid, tanh or ReLU.')
+                raise Exception('Unknown activation function.')
 
-    def train(self) -> None:
+    def train(self, data_dir: str='dataset', batch_size: int=10) -> None:
         '''
         Trains the ANN based on the provided parameters supplied on the constructor.
+
+        Args:
+            batch_size (optional): The number of training examples used before modifying weights. Default: 10.
+            data_dir (optional): The path to the directory containing the training and testing data. Default: 'dataset'.
         '''
-        self.__logger.info(f'Training began with batch size of {self.__batch_size}')
+        self.__logger.info(f'Training began with batch size of {batch_size}')
 
         # Get shuffled training data from data directory.
-        training_data: tuple = utils.shuffle(utils.get_training_data(True, self.__data_dir))
+        training_data: tuple = utils.shuffle(utils.get_training_data(True, data_dir))
         self.__images = training_data[0]
         self.__labels = training_data[1]
 
@@ -50,12 +71,30 @@ class Backpropagation():
         
         # Initialize neurons, weights and biases.
         self.__initialize_network()
-               
+
+        # Train neural network.
+        while len(self.__images) > 0:
+            for _ in range(batch_size):
+                label: int = int(self.__labels[-1])
+                self.__labels = self.__labels[:-1]
+                self.__forward(self.__images.pop())
+                                       
     def test(self) -> None:
         pass
 
-    def __forward(self, image: list, label: int) -> None:
-        pass
+    def __forward(self, image: list) -> None:
+
+        # Normalize values between 0 and 1
+        self.__neurons[0] = np.array(image) / 255
+
+        # Compute neurons value for every layer (including output).
+        for layer in range(1, self.__hidden_layers + 2):
+            self.__neurons[layer] = self.__act_function(
+                np.add(
+                    np.matmul(self.__weights[layer - 1], self.__neurons[layer - 1]),
+                    self.__biases[layer - 1]
+                )
+            )
 
     def __backward(self) -> None:
         pass
@@ -95,12 +134,9 @@ class Backpropagation():
 
             # Create random set of weights and biases between this layer and the next one.
             self.__weights.append(
-                np.random.rand( 
-                    next_layer_size,
-                    this_layer_size
-                )
+                np.random.uniform(-1, 1, (next_layer_size, this_layer_size))
             )
-            self.__biases.append(np.random.rand(this_layer_size))
+            self.__biases.append(np.random.uniform(-1, 1, next_layer_size))
 
         # Establish output layer size.
         self.__neurons.append(np.empty(self.__output_layer))
@@ -108,9 +144,6 @@ class Backpropagation():
         # Create random set of weights and biases between this layer and the output.
         last_layer_size: int = self.__neurons[-2].size
         self.__weights.append(
-            np.random.rand(
-                self.__output_layer,
-                last_layer_size
-            )
+            np.random.uniform(-1, 1, (self.__output_layer, last_layer_size))
         )
-        self.__biases.append(np.random.rand(self.__output_layer))
+        self.__biases.append(np.random.uniform(-1, 1, self.__output_layer))
